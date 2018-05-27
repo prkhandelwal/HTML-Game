85c19821002247d29605bb9dafd7844c6a80f617
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-05-28 01:53:07 +0530

    Setting up CreateJS

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,6 +1,20 @@
 class Game{
     constructor(){
         console.log(`Welcome to the game. Version ${this.Version()}`);
+
+        this.canvas = document.getElementById("game-canvas");
+        this.stage = new createjs.Stage(this.canvas);
+
+        createjs.Ticker.setFPS(60);
+
+        //keep redrawing the stage on every tick
+        createjs.Ticker.on("tick", this.stage);
+        
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("red").drawCircle(0,0,40);
+        circle.x = circle.y = 100;
+        this.stage.addChild(circle);        
+    
     }
 
     Version()
@@ -8,3 +22,6 @@ class Game{
         return '1.0.0';
     }
 }
+
+//Start the game
+var game = new Game();
